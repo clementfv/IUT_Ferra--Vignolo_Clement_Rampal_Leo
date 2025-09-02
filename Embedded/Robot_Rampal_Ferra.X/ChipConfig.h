bcd923a535ecc1fddd03ef236b45a3c569890906
iutgeiitoulonE105 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2025-09-02 16:05:34 +0200

    Installation 2 Mplab

--- Embedded/Robot_Rampal_Ferra.X/ChipConfig.h
+++ Embedded/Robot_Rampal_Ferra.X/ChipConfig.h
@@ -1,30 +1,3 @@
-/* 
- * File:   ChipConfig.h
- * Author: E306-PC4
- *
- * Created on 2 septembre 2025, 16:03
- */
-
-#ifndef CHIPCONFIG_H
-#define	CHIPCONFIG_H
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-
-
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif	/* CHIPCONFIG_H */
-
-/* 
- * File:   ChipConfig.h
- */
-
 #ifndef CHIPCONFIG_H
 #define	CHIPCONFIG_H
 
