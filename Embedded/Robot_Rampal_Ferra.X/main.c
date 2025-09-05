bdcdf1ce420b96acc13b6c665d42ca0af847d8ee
iutgeiitoulonE105 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2025-09-05 11:29:51 +0200

    Initialisation des timers dans le main

--- Embedded/Robot_Rampal_Ferra.X/main.c
+++ Embedded/Robot_Rampal_Ferra.X/main.c
@@ -9,6 +9,8 @@ int main(void) {
 
     InitOscillator();
     InitIO();
+    InitTimer1();
+    InitTimer23();
 
     LED_BLANCHE_1 = 1;
     LED_BLEUE_1 = 1;
@@ -22,10 +24,5 @@ int main(void) {
     LED_VERTE_2 = 1;
     
     while (1) {
-        LED_BLANCHE_1 = !LED_BLANCHE_1 ;
-        LED_BLEUE_1 = !LED_BLEUE_1;
-        LED_ORANGE_1 = !LED_ORANGE_1;
-        LED_ROUGE_1 = !LED_ROUGE_1;
-        LED_VERTE_1 = !LED_VERTE_1;
     } // fin main
 }
