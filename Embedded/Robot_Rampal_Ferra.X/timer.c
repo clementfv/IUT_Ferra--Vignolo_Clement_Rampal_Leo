f052d8b5ea14429f326203d16989c43d9b1991d9
iutgeiitoulonE105 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2025-09-10 09:10:28 +0200

    Fin timers

--- Embedded/Robot_Rampal_Ferra.X/timer.c
+++ Embedded/Robot_Rampal_Ferra.X/timer.c
@@ -5,13 +5,13 @@
 void InitTimer1(void) {
 //Timer1 pour horodater les mesures (1ms)
 T1CONbits.TON = 0; // Disable Timer
-T1CONbits.TCKPS = 0b01; //Prescaler
+T1CONbits.TCKPS = 0b10; //Prescaler
 //11 = 1:256 prescale value
 //10 = 1:64 prescale value
 //01 = 1:8 prescale value
 //00 = 1:1 prescale value
 T1CONbits.TCS = 0; //clock source = internal clock
-PR1 = 60000;
+PR1 = 18750  ;
 IFS0bits.T1IF = 0; // Clear Timer Interrupt Flag
 IEC0bits.T1IE = 1; // Enable Timer interrupt
 T1CONbits.TON = 1; // Enable Timer
@@ -27,11 +27,11 @@ T3CONbits.TON = 0; // Stop any 16-bit Timer3 operation
 T2CONbits.TON = 0; // Stop any 16/32-bit Timer3 operation
 T2CONbits.T32 = 1; // Enable 32-bit Timer mode
 T2CONbits.TCS = 0; // Select internal instruction cycle clock
-T2CONbits.TCKPS = 0b00; // Select 1:1 Prescaler
+T2CONbits.TCKPS = 0b11; // Select 1:1 Prescaler
 TMR3 = 0x00; // Clear 32-bit Timer (msw)
 TMR2 = 0x00; // Clear 32-bit Timer (lsw)
-PR3 = 0x0393; // Load 32-bit period value (msw)
-PR2 = 0x8700; // Load 32-bit period value (lsw)
+PR3 = 0x0007; // Load 32-bit period value (msw)
+PR2 = 0x270E; // Load 32-bit period value (lsw)
 IPC2bits.T3IP = 0x01; // Set Timer3 Interrupt Priority Level
 IFS0bits.T3IF = 0; // Clear Timer3 Interrupt Flag
 IEC0bits.T3IE = 1; // Enable Timer3 interrupt
