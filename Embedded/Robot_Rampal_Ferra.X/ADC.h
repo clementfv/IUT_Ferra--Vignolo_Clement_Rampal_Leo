0f2892fb891a6037f1a02beb03eb1c33f652305c
iutgeiitoulonE105 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2025-09-16 11:15:13 +0200

    Mise en place IDE

---
+++ Embedded/Robot_Rampal_Ferra.X/ADC.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ADC.h
+ * Author: E306-PC4
+ *
+ * Created on 16 septembre 2025, 10:58
+ */
+
+#ifndef ADC_H
+#define	ADC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ADC_H */
+
